29eeeaa1db307d4c09d04c2ee7d7e062a75d8f83
emof <eugene.ofriel@gmail.com>
2020-03-20 14:35:00 -0400

    changing in/out file names for weather-cleaner.bash

--- scripts-bash/weather-cleaner.bash
+++ scripts-bash/weather-cleaner.bash
@@ -1,2 +1,2 @@
 echo "running ../scripts-python/weather-cleaner.py"
-python ../scripts-python/weather-cleaner.py ../data/weather.csv ../data/weather-cleaned.csv
+python ../scripts-python/weather-cleaner.py ../data/weather-raw.csv ../data/weather.csv
